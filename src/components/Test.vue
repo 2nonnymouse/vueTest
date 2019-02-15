42d9dfdf4bab9d23a2532168131da504201395be
2nonnymouse <47648494+2nonnymouse@users.noreply.github.com>
2019-02-14 18:30:08 -0600

    Initial commit

---
+++ src/components/Test.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="test">
+    <!-- input binding -->
+    <input type="text" v-model="title" /><br />
+    <h1>{{ title }}</h1>
+    <p>{{ user.firstName }}</p>
+    <p v-text="user.firstName"></p>
+    <p v-if="showName">{{ user.lastName }}</p>
+    <p v-else="showName">{{ user.firstName }}</p>
+
+    <ul>
+      <li v-for="item in items">{{ item.title }}</li>
+    </ul>
+    <button v-on:click="greet">Say Greeting</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "test",
+  data() {
+    return {
+      title: "Hello world",
+      user: {
+        firstName: "scott",
+        lastName: "Iam"
+      },
+      showName: true,
+      items: [
+        { title: "Item One" },
+        { title: "Item Two" },
+        { title: "Item Three" }
+      ]
+    };
+  },
+  methods: {
+    greet: function() {
+      alert("Hello");
+    }
+  }
+};
+</script>
+
+<style scoped></style>
